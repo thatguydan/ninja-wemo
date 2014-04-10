58cfc57fe1eb3afdb67aa9e1537dc013b5da2c52
☃ Elliot Shepherd <elliot@ninjablocks.com>
2014-04-10 14:56:08 +1000

    Version 0.0.2
    Use 'wemo' nom module.
    Simplify driver (ripped out manual config for now).
    Add test script.

--- index.js
+++ index.js
@@ -1,86 +1,46 @@
-var Socket = require('./lib/socket')
-  , util = require('util')
-  , stream = require('stream')
-  , WeMo = require('wemo.js')
-  , configHandlers = require('./lib/config');
+'use strict';
+
+var Socket = require('./lib/socket');
+var util = require('util');
+var stream = require('stream');
+var WeMo = require('wemo');
 
 // Give our module a stream interface
-util.inherits(wemo,stream);
+util.inherits(Driver,stream);
 
-/**
- * Called when our client starts up
- * @constructor
- *
- * @param  {Object} opts Saved/default module configuration
- * @param  {Object} app  The app event emitter
- * @param  {String} app.id The client serial number
- *
- * @property  {Function} save When called will save the contents of `opts`
- * @property  {Function} config Will be called when config data is received from the cloud
- *
- * @fires register - Emit this when you wish to register a device (see Device)
- * @fires config - Emit this when you wish to send config data back to the cloud
- */
-function wemo(opts,app) {
-
-  var self = this;
+function Driver(opts,app) {
   this._app = app;
   this._opts = opts;
-  this._opts.sockets = opts.sockets || [];
 
-  app.on('client::up', this.init.bind(this));
-};
+  app.once('client::up', this.init.bind(this));
+}
 
 /**
  * Discover and load WeMos
  */
-wemo.prototype.init = function(){
-
+Driver.prototype.init = function(){
   this.scan();
-  // Register known WeMos
-  this._opts.sockets.forEach(this.load.bind(this));
 };
 
-// Export it
-module.exports = wemo;
+Driver.prototype.scan = function() {
 
-wemo.prototype.scan = function() {
+  if (this.search) {
+    this.search.stop();
+  }
+
   // Discover WeMos
-  WeMo.discover(function(WeMos) {
-
-    this._app.log.info('(WeMo) Found %s WeMo(s)',WeMos.length);
-    // Iterate over found WeMos
-    WeMos.forEach(function(WeMo) {
-
-      var host = WeMo.location.host;
-
-      if (this._opts.sockets.indexOf(host)===-1) {
-        this.remember(host);
-      }
-
-      this.load(host);
-
-    }.bind(this));
-
-  }.bind(this));
+  this.search = WeMo.Search();
+  this.search.on('found', this.load.bind(this));
 };
 
-wemo.prototype.load = function(host) {
+Driver.prototype.load = function(device) {
+  this._app.log.info('(WeMo) Found a WeMo - %s (Type: %s) @ %s', device.friendlyName, device.deviceType, device.ip);
 
-  var client = WeMo.createClient(host);
-  var G = this._opts.sockets.indexOf(host);
-
-  this.emit('register',new Socket(this._app,client,G));
-};
-
-/**
- * Add a particular WeMo to this configuration
- * @param {[String} host Host of the WeMo to remember
- */
-wemo.prototype.remember = function(host) {
-
-  this._opts.sockets.push(host);
-  this.save();
+  if (device.deviceType.indexOf('controllee') > -1) {
+    this.emit('register', new Socket(this._app, new WeMo(device.ip, device.port), device));
+  } else {
+    this._app.log.warn('(WeMo) Unknown device (Not a switch?)');
+  }
 };
 
 /**
@@ -88,20 +48,29 @@ wemo.prototype.remember = function(host) {
  * @param  {Object}   rpc     RPC Object
  * @param  {Function} cb      Callback with return data
  */
-wemo.prototype.config = function(rpc,cb) {
-
-  var self = this;
+Driver.prototype.config = function(rpc,cb) {
 
   if (!rpc) {
-    return configHandlers.probe.call(this,cb);
+    return cb(null, {
+      'contents':[
+        { 'type': 'submit', 'name': 'Scan', 'rpc_method': 'configScan' }
+      ]
+    });
   }
 
-  switch (rpc.method) {
-    case 'configScan':               return configHandlers.configScan.call(this,rpc.params,cb); break;
-    case 'manual_set_wemo':    return configHandlers.manual_set_wemo.call(this,rpc.params,cb); break;
-    case 'manual_get_wemo':    return configHandlers.manual_get_wemo.call(this,rpc.params,cb); break;
-    case 'manual_show_remove': return configHandlers.manual_show_remove.call(this,rpc.params,cb); break;
-    case 'manual_remove_wemo': return configHandlers.manual_remove_wemo.call(this,rpc.params,cb); break;
-    default:                   return cb(true);                                              break;
+  if (rpc.method == 'configScan') {
+    return cb(null, {
+      'contents': [
+        { 'type':'paragraph', 'text':'Scanning for WeMo devices. Any devices found will appear shortly.'},
+        { 'type':'close', 'text':'Close'}
+      ]
+    });
+  } else {
+    return cb(true);
   }
+
 };
+
+
+// Export it
+module.exports = Driver;
